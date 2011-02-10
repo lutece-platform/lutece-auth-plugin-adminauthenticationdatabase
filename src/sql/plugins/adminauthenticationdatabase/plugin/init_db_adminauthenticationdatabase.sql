fbce0fa50190eccaf08fbc2b19d6aa4f92699775
DGH <DGH@bab10101-e421-0410-a517-8ce0973de3ef>
2011-02-10 16:02:52 +0000

    Plugins SVN re-organization
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/authentication/plugin-adminauthenticationdatabase@25850 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/sql/plugins/adminauthenticationdatabase/plugin/init_db_adminauthenticationdatabase.sql
@@ -0,0 +1,4 @@
+INSERT INTO core_admin_auth_db_module VALUES ('admin','admin','2006-07-21','x','admin','admin','admin@lutece.fr');
+INSERT INTO core_admin_auth_db_module VALUES ('lutece','lutece','2006-07-21','x','lutece','lutece','lutece@lutece.fr');
+INSERT INTO core_admin_auth_db_module VALUES ('redac','redac','2006-07-21','x','redac','redac','redac@lutece.fr');
+INSERT INTO core_admin_auth_db_module VALUES ('valid','valid','2006-07-21','x','valid','valid','valid@lutece.fr');
